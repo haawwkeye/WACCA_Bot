44ad3298b021a18a1ec90223aa038b11f56dd270
haawwkeye <connorbishop53@gmail.com>
2023-06-25 11:40:28 -0400

    Add global ranks (WIP), add default response

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -14,24 +14,41 @@ function dateToString(date)
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('leaderboard')
-		.setDescription('Replies with a song leaderboard!')
-        .addNumberOption(option => option
-            .setName('userid')
-            .setDescription('The userId you want to get the leaderboard for')
-            .setRequired(false))
-        .addNumberOption(option => option
-            .setName('songid')
-            .setDescription('The songId you want to get the leaderboard for')
-            .setRequired(false)),
+		.setDescription('Replies with a leaderboard you want!')
+        .addSubcommand(command => command
+            .setName("global")
+            .setDescription("Get global ranks based on the players Rate")
+            .addNumberOption(option => option
+                .setName('userid')
+                .setDescription('The userId you want to get the leaderboard for')
+                .setRequired(false))
+        )
+        .addSubcommand(command => command
+            .setName("song")
+            .setDescription("Get a certain songs leaderboard based on songId")
+            .addNumberOption(option => option
+                .setName('songid')
+                .setDescription('The songId you want to get the leaderboard for')
+                .setRequired(true))
+            .addNumberOption(option => option
+                .setName('chartid')
+                .setDescription('The chartId you want to get the leaderboard for')
+                .setChoices(
+                    { name: "Normal", value: 0 },
+                    { name: "Hard", value: 1 },
+                    { name: "Expert", value: 2 },
+                    { name: "INFERNO", value: 3 },
+                )
+                .setRequired(true))
+        ),
 	/**
 	 * @param {ChatInputCommandInteraction} interaction
 	 */
 	async execute(interaction) {
         const client = interaction.client;
-        const query = client.queryDatabase;
-        let uid = interaction.options.getNumber('userid');
-        let sid = interaction.options.getNumber('songid');
-		let database = client.database;
+        const options = interaction.options ?? null;
+        const database = client.database;
+        const query = client.queryDatabase;        
         
         if (!database) return await interaction.reply({ content: 'Failed to connect to artemis database', ephemeral: true });
 
@@ -42,6 +59,34 @@ module.exports = {
 
         // if (rawProfileData.length == 0) return await interaction.reply({ content: `UserId "**${uid}**" not found in artemis database`, ephemeral: true });
 
-        await interaction.reply({ content: 'Leaderboard disabled.', ephemeral: true });
+        if (options?.getSubcommand('global'))
+        {
+            let uid = interaction.options.getNumber('userid');
+            let rawProfileData = await query(database, `SELECT * FROM wacca_profile;`);
+            let index = 0;
+            let lbEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle("Global Ranks")
+
+            rawProfileData.sort((a, b) => b.rating - a.rating);
+            
+            if (rawProfileData.length == 0) lbEmbed.setDescription(`No users found in the database!`);
+            else rawProfileData.forEach(profile => {
+                index++;
+                if (uid == null || uid && profile.user == uid) lbEmbed.addFields({ name: `#${index} ${profile.username}`, value: `Level: **${Math.floor(profile.xp / 100)}**\nXP: **${profile.xp}**\nRate: **${profile.rating}**` });
+            });
+
+            if (uid && lbEmbed.data.fields == null) lbEmbed.setDescription(`UserId "**${uid}**" not found in artemis database`);
+
+            return await interaction.reply({ embeds:[lbEmbed], ephemeral: true });
+        }
+        else if (options?.getSubcommand('song'))  
+        {
+            let sid = interaction.options.getNumber('songid');
+            let cid = interaction.options.getNumber('chartid');
+            // TODO: Add Song support!
+        }
+
+        await interaction.reply({ content: 'Unhandled leaderboard request.', ephemeral: true });
 	},
 };
